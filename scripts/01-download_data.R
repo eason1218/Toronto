3afaa5573d5e6304d1a68df9db4f4b87036ce3ee
YiZhuo Li <52457749+eason1218@users.noreply.github.com>
2024-09-24 14:56:01 -0400

    Update 01-download_data.R

--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -11,18 +11,17 @@
 #### Workspace setup ####
 library(opendatatoronto)
 library(tidyverse)
-# [...UPDATE THIS...]
+
 
 #### Download data ####
 marriage_licence_packages <- search_packages("Daily Shelter & Overnight Service Occupancy & Capacity")
-# Retrieve a list of all resources (datasets) available within the found package
+
 marriage_licence_resources <- marriage_licence_packages %>%
   list_package_resources()
-# Display the list of resources available in the "Marriage Licence Statistics" package
 
 marriage_licence_statistics <- marriage_licence_resources[5,] %>%
   get_resource()
 analysis_data = marriage_licence_statistics
-# 使用 write.csv() 函数保存数据
+
 write.csv(analysis_data, "C:/Users/EasonLi/Downloads/Toronto-main/Toronto-main/data/analysis_data.csv", row.names = FALSE)
 
