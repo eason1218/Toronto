f9b31e586a83d46b521f83d592ac6e1cdd86bf7b
YiZhuo Li <52457749+eason1218@users.noreply.github.com>
2024-09-25 23:30:00 -0400

    Update 00-simulate_data.R

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -1,19 +1,27 @@
 #### Preamble ####
-# Purpose: Simulates... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Simulates table of shelter in Toronto
+# Author: YiZhuo Li
+# Date: Today
+# Contact: liyizhuo.li@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites: No
 
 
 #### Workspace setup ####
 library(tidyverse)
-# [...UPDATE THIS...]
+library(dplyr)
 
-#### Simulate data ####
-# [...ADD CODE HERE...]
-
+set.seed(1)
 
 
+simulated_data <- data.frame(
+  OCCUPANCY_DATE = seq(as.Date('2024-01-01'), by = "day", length.out = 100), 
+  OCCUPANCY_RATE_BEDS = sample(seq(80, 100, by = 0.5), 100, replace = TRUE),  
+  OCCUPANCY_RATE_ROOMS = sample(seq(80, 100, by = 0.5), 100, replace = TRUE),  
+  LOCATION_NAME = sample(c('COSTI Hotel Program Dixon', 'COSTI Reception Centre', 'COSTI Uptown Hotel Program'), 
+                         100, replace = TRUE),  
+  Actual_Bed_Capacity = sample(seq(5, 50, by = 1), 100, replace = TRUE) 
+)
+
+
+head(simulated_data)
