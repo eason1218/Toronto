f104656bbcd78d53957d0ba40a3744042d3bc5d8
YiZhuo Li <52457749+eason1218@users.noreply.github.com>
2024-09-26 15:13:55 -0400

    Update 00-simulate_data.R

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -15,12 +15,14 @@ set.seed(1)
 
 
 simulated_data <- data.frame(
-  OCCUPANCY_DATE = seq(as.Date('2024-01-01'), by = "day", length.out = 100), 
-  OCCUPANCY_RATE_BEDS = sample(seq(80, 100, by = 0.5), 100, replace = TRUE),  
-  OCCUPANCY_RATE_ROOMS = sample(seq(80, 100, by = 0.5), 100, replace = TRUE),  
-  LOCATION_NAME = sample(c('COSTI Hotel Program Dixon', 'COSTI Reception Centre', 'COSTI Uptown Hotel Program'), 
-                         100, replace = TRUE),  
-  Actual_Bed_Capacity = sample(seq(5, 50, by = 1), 100, replace = TRUE) 
+  OCCUPANCY_DATE = seq(as.Date("2024-01-01"), by = "day", length.out = 100),
+  OCCUPANCY_RATE_BEDS = sample(seq(80, 100, by = 0.5), 100, replace = TRUE),
+  OCCUPANCY_RATE_ROOMS = sample(seq(80, 100, by = 0.5), 100, replace = TRUE),
+  LOCATION_NAME = sample(c("COSTI Hotel Program Dixon", "COSTI Reception Centre", "COSTI Uptown Hotel Program"),
+    100,
+    replace = TRUE
+  ),
+  Actual_Bed_Capacity = sample(seq(5, 50, by = 1), 100, replace = TRUE)
 )
 
 
