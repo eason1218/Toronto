54f28e2c20cc10de59a63c8e8c1eab79ee0c128e
YiZhuo Li <52457749+eason1218@users.noreply.github.com>
2024-09-26 15:15:55 -0400

    Update 03-test_data.R

--- scripts/03-test_data.R
+++ scripts/03-test_data.R
@@ -1,5 +1,5 @@
 #### Preamble ####
-# Purpose: Tests clean_data data no obvious statistic mistakes. 
+# Purpose: Tests clean_data data no obvious statistic mistakes.
 # Author: YiZhuo Li
 # Date: Today
 # Contact: liyizhuo.li@mail.utoronto.ca
@@ -12,12 +12,12 @@ library(tidyverse)
 
 data <- read_csv("C:\\Users\\EasonLi\\Downloads\\Toronto-main\\Toronto-main\\data\\analysis_data_clean.csv")
 
-#test 1 date
+# test 1 date
 date_format_test <- all(grepl("^\\d{4}-\\d{2}-\\d{2}$", data$OCCUPANCY_DATE))
 
-#test 2 rate > 0 
+# test 2 rate > 0
 rate_beds_test <- all(data$OCCUPANCY_RATE_BEDS[!is.na(data$OCCUPANCY_RATE_BEDS)] > 0)
 rate_rooms_test <- all(data$OCCUPANCY_RATE_ROOMS[!is.na(data$OCCUPANCY_RATE_ROOMS)] > 0)
 
-#test 3 capicity > 0
+# test 3 capicity > 0
 capacity_test <- all(data$`Actual Bed Capacity`[!is.na(data$`Actual Bed Capacity`)] > 0)
