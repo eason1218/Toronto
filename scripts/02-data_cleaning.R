e20da913def192dc236fe3ca1f168b836620f91f
YiZhuo Li <52457749+eason1218@users.noreply.github.com>
2024-09-26 15:17:02 -0400

    Update 02-data_cleaning.R

--- scripts/02-data_cleaning.R
+++ scripts/02-data_cleaning.R
@@ -12,21 +12,23 @@
 library(tidyverse)
 
 #### Cleaning the Dataset ####
-shelter_raw_data <- read_csv("C:/Users/EasonLi/Downloads/Toronto-main/Toronto-main/data/analysis_data.csv")
+shelter_raw_data <- read_csv("C:/Users/EasonLi/Downloads/Toronto-main/Toronto-main/data/raw_data.csv")
 
-shelter_cleaned_data <- 
+shelter_cleaned_data <-
   shelter_raw_data |>
-  
-  select(`OCCUPANCY_DATE`, `OCCUPANCY_RATE_BEDS`, 
-         `OCCUPANCY_RATE_ROOMS`, `LOCATION_NAME`, `CAPACITY_ACTUAL_BED`) |>
-  
-  rename(`OCCUPANCY_DATE` = `OCCUPANCY_DATE`,
-         `OCCUPANCY_RATE_BEDS` = `OCCUPANCY_RATE_BEDS`,
-         `OCCUPANCY_RATE_ROOMS` = `OCCUPANCY_RATE_ROOMS`,
-         `LOCATION_NAME` = `LOCATION_NAME`,
-         `Actual Bed Capacity` = `CAPACITY_ACTUAL_BED`)
+  select(
+    `OCCUPANCY_DATE`, `OCCUPANCY_RATE_BEDS`,
+    `OCCUPANCY_RATE_ROOMS`, `LOCATION_NAME`, `CAPACITY_ACTUAL_BED`
+  ) |>
+  rename(
+    `OCCUPANCY_DATE` = `OCCUPANCY_DATE`,
+    `OCCUPANCY_RATE_BEDS` = `OCCUPANCY_RATE_BEDS`,
+    `OCCUPANCY_RATE_ROOMS` = `OCCUPANCY_RATE_ROOMS`,
+    `LOCATION_NAME` = `LOCATION_NAME`,
+    `Actual Bed Capacity` = `CAPACITY_ACTUAL_BED`
+  )
 
-  
+
 #### Saving the Cleaned Dataset ####
-
+# 使用 write.csv()
 write.csv(shelter_cleaned_data, "C:/Users/EasonLi/Downloads/Toronto-main/Toronto-main/data/analysis_data_clean.csv", row.names = FALSE)
